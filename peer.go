f968ecbe39050416411d756832dbbce273390b2e
Omer Akram <omer@thing.com>
2024-05-30 02:48:46 +0500

    add websocket peer implementation

--- peer.go
+++ peer.go
@@ -46,3 +46,54 @@ func (b *baseSession) Read() ([]byte, error) {
 func (b *baseSession) Write(payload []byte) error {
 	return b.client.Write(payload)
 }
+
+func NewWebSocketPeer(conn net.Conn, protocol string, binary, server bool) (Peer, error) {
+	var wsReader ReaderFunc
+	var wsWriter WriterFunc
+	var err error
+	if server {
+		wsReader, wsWriter, err = ServerSideWSReaderWriter(binary)
+	} else {
+		wsReader, wsWriter, err = ClientSideWSReaderWriter(binary)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &WebSocketPeer{
+		transportType: TransportWebSocket,
+		protocol:      protocol,
+		conn:          conn,
+		wsReader:      wsReader,
+		wsWriter:      wsWriter,
+	}, nil
+}
+
+type WebSocketPeer struct {
+	transportType TransportType
+	protocol      string
+	conn          net.Conn
+	wsReader      ReaderFunc
+	wsWriter      WriterFunc
+}
+
+func (c *WebSocketPeer) Read() ([]byte, error) {
+	return c.wsReader(c.conn)
+}
+
+func (c *WebSocketPeer) Write(bytes []byte) error {
+	return c.wsWriter(c.conn, bytes)
+}
+
+func (c *WebSocketPeer) Type() TransportType {
+	return c.transportType
+}
+
+func (c *WebSocketPeer) Protocol() string {
+	return c.protocol
+}
+
+func (c *WebSocketPeer) NetConn() net.Conn {
+	return c.conn
+}
