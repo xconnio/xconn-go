e4c7ae8adddb6978f8b5fbe473d2a728e64de9b8
Omer Akram <omer@thing.com>
2024-06-01 04:19:08 +0500

    add helpers to read and write WAMP messages

--- helpers.go
+++ helpers.go
@@ -1,6 +1,13 @@
 package xconn
 
-import "github.com/gobwas/ws/wsutil"
+import (
+	"fmt"
+
+	"github.com/gobwas/ws/wsutil"
+
+	"github.com/xconnio/wampproto-go/messages"
+	"github.com/xconnio/wampproto-go/serializers"
+)
 
 func ClientSideWSReaderWriter(binary bool) (ReaderFunc, WriterFunc, error) {
 	if !binary {
@@ -16,4 +23,45 @@ func ServerSideWSReaderWriter(binary bool) (ReaderFunc, WriterFunc, error) {
 	}
 
 	return wsutil.ReadClientBinary, wsutil.WriteServerBinary, nil
+}
+
+func ReadMessage(peer Peer, serializer serializers.Serializer) (messages.Message, error) {
+	payload, err := peer.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	msg, err := serializer.Deserialize(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func ReadHello(peer Peer, serializer serializers.Serializer) (*messages.Hello, error) {
+	msg, err := ReadMessage(peer, serializer)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg.Type() != messages.MessageTypeHello {
+		return nil, fmt.Errorf("first message must be HELLO, but was %d", msg.Type())
+	}
+
+	hello := msg.(*messages.Hello)
+	return hello, nil
+}
+
+func WriteMessage(peer Peer, message messages.Message, serializer serializers.Serializer) error {
+	payload, err := serializer.Serialize(message)
+	if err != nil {
+		return fmt.Errorf("failed to serialize message: %w", err)
+	}
+
+	if err = peer.Write(payload); err != nil {
+		return fmt.Errorf("failed to write message: %w", err)
+	}
+
+	return nil
 }
