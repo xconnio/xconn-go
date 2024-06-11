d7d9e612409b7a8b5a69d2fbe9a3c51fa28fa98d
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-11 21:02:27 +0500

    cmd: Add command to start server

--- cmd/xconn/main.go
+++ cmd/xconn/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
 	_ "embed" // nolint:gci
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/alecthomas/kingpin/v2"
+	"golang.org/x/exp/slices"
+	"gopkg.in/yaml.v3"
+
+	"github.com/xconnio/wampproto-protobuf/go"
+	"github.com/xconnio/xconn-go"
 )
 
 var (
@@ -19,12 +25,16 @@ const (
 
 	ConfigDir  = ".xconn"
 	ConfigFile = ConfigDir + "/config.yaml"
+
+	ProtobufSubProtocol = "wamp.2.protobuf"
 )
 
 type cmd struct {
 	parsedCommand string
 
 	init *kingpin.CmdClause
+
+	start *kingpin.CmdClause
 }
 
 func parseCommand(args []string) (*cmd, error) {
@@ -32,7 +42,8 @@ func parseCommand(args []string) (*cmd, error) {
 	app.Version(versionString).VersionFlag.Short('v')
 
 	c := &cmd{
-		init: app.Command("init", "Initialize sample router config."),
+		init:  app.Command("init", "Initialize sample router config."),
+		start: app.Command("start", "Start the router."),
 	}
 
 	parsedCommand, err := app.Parse(args[1:])
@@ -59,6 +70,44 @@ func Run(args []string) error {
 		if err = os.WriteFile(ConfigFile, sampleConfig, 0600); err != nil {
 			return fmt.Errorf("unable to write config: %w", err)
 		}
+
+	case c.start.FullCommand():
+		data, err := os.ReadFile(ConfigFile)
+		if err != nil {
+			return fmt.Errorf("unable to read config file: %w", err)
+		}
+
+		var decoder = yaml.NewDecoder(bytes.NewBuffer(data))
+		decoder.KnownFields(true)
+
+		var config Config
+		if err := decoder.Decode(&config); err != nil {
+			return fmt.Errorf("unable to decode config file: %w", err)
+		}
+
+		router := xconn.NewRouter()
+
+		for _, realm := range config.Realms {
+			router.AddRealm(realm.Name)
+		}
+
+		server := xconn.NewServer(router, nil)
+
+		for _, transport := range config.Transports {
+			if slices.Contains(transport.Serializers, "protobuf") {
+				serializer := &wampprotobuf.ProtobufSerializer{}
+				protobufSpec := xconn.NewWSSerializerSpec(ProtobufSubProtocol, serializer)
+
+				if err := server.RegisterSpec(protobufSpec); err != nil {
+					return err
+				}
+			}
+
+			if err := server.Start("0.0.0.0", transport.Port); err != nil {
+				return err
+			}
+		}
+
 	}
 
 	return nil
