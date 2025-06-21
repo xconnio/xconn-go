ff079cedcc77618957d0b30f86e46c10bf9a78f7
Omer Akram <om26er@genki.com>
2025-06-21 16:39:52 +0500

    add unix socket and rawsocket server example

---
+++ examples/server/unixsocket/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/xconnio/xconn-go"
+)
+
+func main() {
+	path := flag.String("path", "xconn.sock", "path to listen on")
+	realm := flag.String("realm", "realm1", "realm to use")
+	help := flag.Bool("help", false, "print help")
+
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	r := xconn.NewRouter()
+	r.AddRealm(*realm)
+	defer r.Close()
+
+	server := xconn.NewServer(r, nil, nil)
+	closer, err := server.StartUnixSocket(*path)
+	if err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+	defer closer.Close()
+
+	// Close server if SIGINT (CTRL-c) received.
+	closeChan := make(chan os.Signal, 1)
+	signal.Notify(closeChan, os.Interrupt)
+	<-closeChan
+}
