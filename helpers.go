3cd20d3cf31811e905f1f087541b41afd7d36532
Omer Akram <omer@xconn.io>
2025-10-31 15:19:35 +0500

    trace: include timestamp and direction in logs (#241)

--- helpers.go
+++ helpers.go
@@ -2,6 +2,7 @@ package xconn
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/xconnio/wampproto-go/messages"
 	"github.com/xconnio/wampproto-go/serializers"
@@ -104,97 +105,109 @@ func messageNameByID(id uint64) string {
 }
 
 func constructReceivedMsgLog(msg messages.Message) string {
+	timestamp := time.Now().Format("2006-01-02 15:04:05.000")
 	switch msg.Type() {
 	case messages.MessageTypeCall:
 		callMsg := msg.(*messages.Call)
-		return fmt.Sprintf("Received %s for procedure %s with request_id=%v args=%v, kwargs%v, options=%v",
-			messages.MessageNameCall, callMsg.Procedure(), callMsg.RequestID(), callMsg.Args(), callMsg.KwArgs(),
-			callMsg.Options())
+		return fmt.Sprintf("[%s] RX: %s for procedure %s with request_id=%v args=%v, kwargs=%v, options=%v",
+			timestamp, messages.MessageNameCall, callMsg.Procedure(), callMsg.RequestID(),
+			callMsg.Args(), callMsg.KwArgs(), callMsg.Options())
 	case messages.MessageTypeYield:
 		yieldMsg := msg.(*messages.Yield)
-		return fmt.Sprintf("Received %s with request_id=%v args=%v, kwargs=%v, options=%v",
-			messages.MessageNameYield, yieldMsg.RequestID(), yieldMsg.Args(), yieldMsg.KwArgs(), yieldMsg.Options())
+		return fmt.Sprintf("[%s] RX: %s with request_id=%v args=%v, kwargs=%v, options=%v",
+			timestamp, messages.MessageNameYield, yieldMsg.RequestID(),
+			yieldMsg.Args(), yieldMsg.KwArgs(), yieldMsg.Options())
 	case messages.MessageTypeRegister:
 		registerMsg := msg.(*messages.Register)
-		return fmt.Sprintf("Received %s for procedure=%s with request_id=%v, options=%v",
-			messages.MessageNameRegister, registerMsg.Procedure(), registerMsg.RequestID(), registerMsg.Options())
+		return fmt.Sprintf("[%s] RX: %s for procedure=%s with request_id=%v, options=%v",
+			timestamp, messages.MessageNameRegister, registerMsg.Procedure(),
+			registerMsg.RequestID(), registerMsg.Options())
 	case messages.MessageTypeUnregister:
 		unregisterMsg := msg.(*messages.Unregister)
-		return fmt.Sprintf("Received %s with request_id=%v for registration=%v",
-			messages.MessageNameUnregister, unregisterMsg.RequestID(), unregisterMsg.RegistrationID())
+		return fmt.Sprintf("[%s] RX: %s with request_id=%v for registration=%v",
+			timestamp, messages.MessageNameUnregister, unregisterMsg.RequestID(),
+			unregisterMsg.RegistrationID())
 	case messages.MessageTypePublish:
 		publishMsg := msg.(*messages.Publish)
-		return fmt.Sprintf("Received %s for topic %s with args=%v, kwargs=%v, options=%v",
-			messages.MessageNamePublish, publishMsg.Topic(), publishMsg.Args(), publishMsg.KwArgs(), publishMsg.Options())
+		return fmt.Sprintf("[%s] RX: %s for topic %s with args=%v, kwargs=%v, options=%v",
+			timestamp, messages.MessageNamePublish, publishMsg.Topic(),
+			publishMsg.Args(), publishMsg.KwArgs(), publishMsg.Options())
 	case messages.MessageTypeSubscribe:
 		subscribeMsg := msg.(*messages.Subscribe)
-		return fmt.Sprintf("Received %s for topic=%s with request_id=%v, options=%v",
-			messages.MessageNameSubscribe, subscribeMsg.Topic(), subscribeMsg.RequestID(), subscribeMsg.Options())
+		return fmt.Sprintf("[%s] RX: %s for topic=%s with request_id=%v, options=%v",
+			timestamp, messages.MessageNameSubscribe, subscribeMsg.Topic(),
+			subscribeMsg.RequestID(), subscribeMsg.Options())
 	case messages.MessageTypeUnsubscribe:
 		unsubscribeMsg := msg.(*messages.Unsubscribe)
-		return fmt.Sprintf("Received %s with request_id=%v for subscription=%v",
-			messages.MessageNameUnsubscribe, unsubscribeMsg.RequestID(), unsubscribeMsg.SubscriptionID())
+		return fmt.Sprintf("[%s] RX: %s with request_id=%v for subscription=%v",
+			timestamp, messages.MessageNameUnsubscribe, unsubscribeMsg.RequestID(),
+			unsubscribeMsg.SubscriptionID())
 	case messages.MessageTypeError:
 		errorMsg := msg.(*messages.Error)
-		return fmt.Sprintf("Received %s for message %v with uri=%s, args=%v, kwargs%v, details=%v",
-			messages.MessageNameError, errorMsg.MessageType(), errorMsg.URI(), errorMsg.Args(), errorMsg.KwArgs(),
-			errorMsg.Details())
+		return fmt.Sprintf("[%s] RX: %s for message %v with uri=%s, args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameError, errorMsg.MessageType(), errorMsg.URI(),
+			errorMsg.Args(), errorMsg.KwArgs(), errorMsg.Details())
 	case messages.MessageTypeGoodbye:
 		goodbyeMsg := msg.(*messages.GoodBye)
-		return fmt.Sprintf("Received %s with reason %s", messages.MessageNameGoodbye, goodbyeMsg.Reason())
+		return fmt.Sprintf("[%s] RX: %s with reason %s",
+			timestamp, messages.MessageNameGoodbye, goodbyeMsg.Reason())
 	default:
-		return fmt.Sprintf("Received %+v", msg.Marshal())
+		return fmt.Sprintf("[%s] RX: %+v", timestamp, msg.Marshal())
 	}
 }
 
 func constructSendingMsgLog(msg messages.Message) string {
+	timestamp := time.Now().Format("2006-01-02 15:04:05.000")
 	switch msg.Type() {
 	case messages.MessageTypeResult:
 		result := msg.(*messages.Result)
-		return fmt.Sprintf("Sending %s for with request_id=%v args=%v, kwargs%v, details=%v",
-			messages.MessageNameResult, result.RequestID(), result.Args(), result.KwArgs(), result.Details())
+		return fmt.Sprintf("[%s] TX: %s for with request_id=%v args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameResult, result.RequestID(), result.Args(), result.KwArgs(), result.Details())
 	case messages.MessageTypeInvocation:
 		invocation := msg.(*messages.Invocation)
-		return fmt.Sprintf("Sending %s with request_id=%v registration_id=%v args=%v, kwargs=%v, details=%v",
-			messages.MessageNameInvocation, invocation.RequestID(), invocation.RegistrationID(), invocation.Args(),
-			invocation.KwArgs(), invocation.Details())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v registration_id=%v args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameInvocation, invocation.RequestID(), invocation.RegistrationID(),
+			invocation.Args(), invocation.KwArgs(), invocation.Details())
 	case messages.MessageTypeRegistered:
 		registered := msg.(*messages.Registered)
-		return fmt.Sprintf("Sending %s with request_id=%v, regitration_id=%v",
-			messages.MessageNameRegistered, registered.RequestID(), registered.RegistrationID())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v, registration_id=%v",
+			timestamp, messages.MessageNameRegistered, registered.RequestID(), registered.RegistrationID())
 	case messages.MessageTypeUnregistered:
 		unregistered := msg.(*messages.Unregistered)
-		return fmt.Sprintf("Sending %s with request_id=%v",
-			messages.MessageNameUnregistered, unregistered.RequestID())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v",
+			timestamp, messages.MessageNameUnregistered, unregistered.RequestID())
 	case messages.MessageTypeEvent:
 		event := msg.(*messages.Event)
-		return fmt.Sprintf("Sending %s with publication_id=%v subcription_id=%v args=%v, kwargs=%v, details=%v",
-			messages.MessageNameEvent, event.PublicationID(), event.SubscriptionID(), event.Args(),
-			event.KwArgs(), event.Details())
+		return fmt.Sprintf("[%s] TX: %s with publication_id=%v subscription_id=%v args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameEvent, event.PublicationID(), event.SubscriptionID(),
+			event.Args(), event.KwArgs(), event.Details())
 	case messages.MessageTypePublished:
 		published := msg.(*messages.Published)
-		return fmt.Sprintf("Sending %s with request_id=%v, publication_id=%v",
-			messages.MessageNamePublished, published.RequestID(), published.PublicationID())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v, publication_id=%v",
+			timestamp, messages.MessageNamePublished, published.RequestID(), published.PublicationID())
 	case messages.MessageTypeSubscribed:
 		subscribed := msg.(*messages.Subscribed)
-		return fmt.Sprintf("Sending %s with request_id=%v, subscription_id=%v",
-			messages.MessageNameSubscribed, subscribed.RequestID(), subscribed.SubscriptionID())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v, subscription_id=%v",
+			timestamp, messages.MessageNameSubscribed, subscribed.RequestID(), subscribed.SubscriptionID())
 	case messages.MessageTypeUnsubscribed:
 		unsubscribed := msg.(*messages.Unsubscribed)
-		return fmt.Sprintf("Sending %s with request_id=%v", messages.MessageNameUnsubscribed, unsubscribed.RequestID())
+		return fmt.Sprintf("[%s] TX: %s with request_id=%v",
+			timestamp, messages.MessageNameUnsubscribed, unsubscribed.RequestID())
 	case messages.MessageTypeError:
 		errorMsg := msg.(*messages.Error)
-		return fmt.Sprintf("Sending %s for message %v with uri=%s, args=%v, kwargs%v, details=%v",
-			messages.MessageNameError, errorMsg.MessageType(), errorMsg.URI(), errorMsg.Args(), errorMsg.KwArgs(),
-			errorMsg.Details())
+		return fmt.Sprintf("[%s] TX: %s for message %v with uri=%s, args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameError, errorMsg.MessageType(), errorMsg.URI(),
+			errorMsg.Args(), errorMsg.KwArgs(), errorMsg.Details())
 	case messages.MessageTypeGoodbye:
 		goodbyeMsg := msg.(*messages.GoodBye)
-		return fmt.Sprintf("Sending %s with reason %s", messages.MessageNameGoodbye, goodbyeMsg.Reason())
+		return fmt.Sprintf("[%s] TX: %s with reason %s",
+			timestamp, messages.MessageNameGoodbye, goodbyeMsg.Reason())
 	case messages.MessageTypeAbort:
 		abort := msg.(*messages.Abort)
-		return fmt.Sprintf("Sending %s with reason %s, args=%v, kwargs%v, details=%v", messages.MessageNameAbort,
-			abort.Reason(), abort.Args(), abort.KwArgs(), abort.Details())
+		return fmt.Sprintf("[%s] TX: %s with reason %s, args=%v, kwargs=%v, details=%v",
+			timestamp, messages.MessageNameAbort, abort.Reason(),
+			abort.Args(), abort.KwArgs(), abort.Details())
 	default:
-		return fmt.Sprintf("Sending %+v", msg.Marshal())
+		return fmt.Sprintf("[%s] TX: %+v", timestamp, msg.Marshal())
 	}
 }
